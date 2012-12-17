69d91c1c805d011a6a73d1d41d1842a3c122727d
dailycraft <server@daily-craft.de>
2012-12-17 19:46:08 +0100

    make ejb to webservice

--- EJB/src/main/java/de/minecraftadmin/ejb/beans/BanService.java
+++ EJB/src/main/java/de/minecraftadmin/ejb/beans/BanService.java
@@ -1,25 +1,39 @@
 package de.minecraftadmin.ejb.beans;
 
 import de.minecraftadmin.api.API;
-import de.minecraftadmin.ejb.authentication.AuthenticationManager;
+import de.minecraftadmin.api.entity.Player;
 import de.minecraftadmin.api.entity.PlayerBan;
+import de.minecraftadmin.ejb.authentication.AuthenticationManager;
 
 import javax.ejb.EJB;
+import javax.ejb.Remote;
 import javax.interceptor.Interceptors;
+import javax.jws.WebService;
 
 /**
+ * @author BADMAN152
  * implements the remote api service
  */
 @EJB
+@WebService(
+        portName = "RemoteApiPort",
+        serviceName = "RemoteApi",
+        targetNamespace = "http://minecraftadmin.de/secureban",
+        endpointInterface = "de.minecraftadmin.api.API")
+@Remote(value = API.class)
 @Interceptors(value = AuthenticationManager.class)
 public class BanService implements API {
+
+    @EJB
+    private DatabaseService database;
+
     @Override
-    public PlayerBan getPlayerBans(String userName) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public Player getPlayerBans(String userName) {
+        return null;
     }
 
     @Override
-    public void submitPlayerBans(PlayerBan playerBan) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void submitPlayerBans(Player playerBan, PlayerBan ban) {
+
     }
 }
