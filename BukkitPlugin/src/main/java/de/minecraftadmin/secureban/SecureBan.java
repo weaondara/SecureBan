d0538ca9799f4903f554bbf9c4f254e582fba337
dailycraft <server@daily-craft.de>
2012-12-17 19:30:56 +0100

    startup mainclass of bukkitplugin

---
+++ BukkitPlugin/src/main/java/de/minecraftadmin/secureban/SecureBan.java
@@ -0,0 +1,23 @@
+package de.minecraftadmin.secureban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dustin
+ * Date: 17.12.12
+ * Time: 18:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class SecureBan extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
