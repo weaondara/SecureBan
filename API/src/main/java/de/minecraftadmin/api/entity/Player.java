c0cf4d767151ca0e4c73939701f1df8b9e012072
dailycraft <server@daily-craft.de>
2012-12-17 19:10:44 +0100

    persist each player

---
+++ API/src/main/java/de/minecraftadmin/api/entity/Player.java
@@ -0,0 +1,43 @@
+package de.minecraftadmin.api.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * @author BADMAN152
+ * represent a player
+ */
+@Entity
+public class Player {
+
+    @Id
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String userName;
+    @OneToMany(cascade = {CascadeType.ALL},fetch = FetchType.EAGER)
+    private Set<PlayerBan> bans;
+
+    public Set<PlayerBan> getBans() {
+        return bans;
+    }
+
+    public void setBans(Set<PlayerBan> bans) {
+        this.bans = bans;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
