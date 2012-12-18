7ed103bc2d3dabb0a29a8db49e4e695819bb80cf
dailycraft <server@daily-craft.de>
2012-12-18 15:06:56 +0100

    authentication now got tested

--- EJB/src/test/java/webservice/WebserviceTest.java
+++ EJB/src/test/java/webservice/WebserviceTest.java
@@ -2,78 +2,94 @@ package webservice;
 
 import de.minecraftadmin.api.API;
 import de.minecraftadmin.api.RemoteAPIManager;
-import de.minecraftadmin.api.entity.BanType;
-import de.minecraftadmin.api.entity.Player;
-import de.minecraftadmin.api.entity.PlayerBan;
-import de.minecraftadmin.api.entity.SaveState;
+import de.minecraftadmin.api.entity.*;
+import de.minecraftadmin.ejb.beans.DatabaseService;
+import org.apache.openejb.api.LocalClient;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.ejb.embeddable.EJBContainer;
+import javax.xml.ws.soap.SOAPFaultException;
 import java.util.Properties;
 
 /**
  *
  */
+@LocalClient
 public class WebserviceTest {
     private static final String serviceURL = "http://127.0.0.1:4204/localhost/SecureBan/BanService";
     private static EJBContainer container;
     private static RemoteAPIManager remote;
 
     @BeforeClass
-    public static void startup()throws Throwable{
+    public static void startup() throws Throwable {
         Properties properties = new Properties();
         properties.setProperty("httpejbd.print", "true");
         properties.setProperty("httpejbd.indent.xml", "true");
         properties.setProperty("openejb.embedded.remotable", "true");
         container = EJBContainer.createEJBContainer(properties);
-        remote = new RemoteAPIManager(serviceURL,"apikey");
+        DatabaseService service = (DatabaseService) container.getContext().lookup("java:global/localhost/SecureBan/DatabaseService");
+        Server server = new Server();
+        server.setServerName("JUnitServer");
+        server.setApiKey("apikey");
+        service.persist(server);
+        remote = new RemoteAPIManager(serviceURL, "apikey");
         Assert.assertNotNull(remote.getRemoteAPI());
     }
 
-    public API getWebservice() throws Throwable{
+    public API getWebservice() throws Throwable {
         return remote.getRemoteAPI();
     }
 
+    public API getFailedWebservice() throws Throwable {
+        RemoteAPIManager manager = new RemoteAPIManager(serviceURL, "blabla");
+        return manager.getRemoteAPI();
+    }
+
     @AfterClass
-    public static void shutdown() throws Throwable{
+    public static void shutdown() throws Throwable {
         container.close();
     }
 
+    @Test(expected = SOAPFaultException.class)
+    public void notAuthenticated() throws Throwable {
+        getFailedWebservice().submitPlayerBans("", null);
+    }
+
     @Test
-    public void getCleanUser()throws Throwable{
+    public void getCleanUser() throws Throwable {
         Player p = getWebservice().getPlayerBans("JUnitTestUser");
         Assert.assertNotNull(p);
         Assert.assertNull("Has no bans", p.getBans());
     }
 
     @Test
-    public void getNewAddedBan()throws Throwable{
+    public void getNewAddedBan() throws Throwable {
         PlayerBan ban = new PlayerBan();
         ban.setBanReason("Cause I Can");
         ban.setStaffName("StaffJUnitTestUser");
         ban.setBanType(BanType.GLOBAL);
         ban.setSaveState(SaveState.SAVED);
-        getWebservice().submitPlayerBans("JUnitTestUser",ban);
+        getWebservice().submitPlayerBans("JUnitTestUser", ban);
         Player p = getWebservice().getPlayerBans("JUnitTestUser");
         Assert.assertNotNull(p);
         Assert.assertNotNull("Has bans", p.getBans());
-        Assert.assertEquals("Has one ban",1,p.getBans().size());
+        Assert.assertEquals("Has one ban", 1, p.getBans().size());
     }
 
     @Test
-    public void getNewSecondBan() throws Throwable{
+    public void getNewSecondBan() throws Throwable {
         PlayerBan ban = new PlayerBan();
         ban.setBanReason("Cause I Can, too");
         ban.setStaffName("StaffJUnitTestUser");
         ban.setBanType(BanType.GLOBAL);
         ban.setSaveState(SaveState.SAVED);
-        getWebservice().submitPlayerBans("JUnitTestUser",ban);
+        getWebservice().submitPlayerBans("JUnitTestUser", ban);
         Player p = getWebservice().getPlayerBans("JUnitTestUser");
         Assert.assertNotNull(p);
         Assert.assertNotNull("Has bans", p.getBans());
-        Assert.assertEquals("Has two ban",2,p.getBans().size());
+        Assert.assertEquals("Has two ban", 2, p.getBans().size());
     }
 }
