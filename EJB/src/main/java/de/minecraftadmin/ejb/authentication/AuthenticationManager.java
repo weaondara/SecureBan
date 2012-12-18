0e44a706e586a4b8542d5aa8784ac739c7d35a8a
dailycraft <server@daily-craft.de>
2012-12-18 14:07:43 +0100

    implemented full functional authentication for webservice

--- EJB/src/main/java/de/minecraftadmin/ejb/authentication/AuthenticationManager.java
+++ EJB/src/main/java/de/minecraftadmin/ejb/authentication/AuthenticationManager.java
@@ -1,20 +1,28 @@
 package de.minecraftadmin.ejb.authentication;
 
+import de.minecraftadmin.api.entity.Server;
+import de.minecraftadmin.ejb.beans.DatabaseService;
+import org.apache.log4j.Logger;
+
 import javax.annotation.Resource;
+import javax.ejb.EJB;
 import javax.interceptor.AroundInvoke;
-import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * authenticate each call via api-Key
  */
-@Interceptor
 public class AuthenticationManager {
+
+    private final Logger LOG = Logger.getLogger("SecurityManager");
+    @EJB
+    private DatabaseService databaseService;
     @Resource
     private WebServiceContext webservice;
 
@@ -24,9 +32,17 @@ public class AuthenticationManager {
         Map headerData = (Map) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
         HttpServletRequest req = (HttpServletRequest) messageContext.get(MessageContext.SERVLET_REQUEST);
         List keys = (List) headerData.get("apikey");
+        Server s = null;
+        String apikey = "NONE";
         if (keys != null) {
-            String password = keys.get(0).toString();
+            apikey = keys.get(0).toString();
+            HashMap<String, Object> param = new HashMap<String, Object>();
+            param.put("key", apikey);
+            s = databaseService.getSingleResult(Server.class, "SELECT s FROM Server s WHERE s.apiKey=:key", param);
         }
+        if (s == null) throw new SecurityException("Could not Authenticate request with apiKey: " + apikey);
+        LOG.info("Authenticated Server "+s.getServerName());
+
 
         return ic.proceed();
     }
