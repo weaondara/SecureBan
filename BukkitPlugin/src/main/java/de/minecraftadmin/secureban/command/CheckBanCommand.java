b68f40d54a27b99a31fe96cbf8713c44e690394a
dailycraft <server@daily-craft.de>
2012-12-24 17:22:46 +0100

    bug fix: Exception while getting all bans

--- BukkitPlugin/src/main/java/de/minecraftadmin/secureban/command/CheckBanCommand.java
+++ BukkitPlugin/src/main/java/de/minecraftadmin/secureban/command/CheckBanCommand.java
@@ -44,8 +44,10 @@ public class CheckBanCommand implements CommandExecutor {
         String userName = args[0];
         Player p = banManager.getAllBansOfPlayer(userName);
         sender.sendMessage(ChatColor.WHITE + "[SecureBan]" + ChatColor.UNDERLINE + ChatColor.GOLD + "All known ban's for " + p.getUserName());
-        if (p.getBans().isEmpty())
+        if (p.getBans() == null) {
             sender.sendMessage(ChatColor.WHITE + "[SecureBan] No bans");
+            return true;
+        }
         for (PlayerBan ban : p.getBans()) {
             if (ban.getExpired() != null) {
                 if (new Date().after(new Date(ban.getExpired())))
