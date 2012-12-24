f1cd821b94a933d18f1066f38349ecf4bf7f5e8d
dailycraft <server@daily-craft.de>
2012-12-24 23:41:34 +0100

    bugfix: should no longer throw synchronization exception while commit new bans/update bans

--- EJB/src/main/java/de/minecraftadmin/ejb/beans/BanService.java
+++ EJB/src/main/java/de/minecraftadmin/ejb/beans/BanService.java
@@ -58,6 +58,7 @@ public class BanService implements API {
     public void submitPlayerBans(final String playerName, PlayerBan ban) {
         ban.setServer(getRequestedServer());
         ban.setSaveState(SaveState.SAVED);
+        ban.setId(null);
         HashMap<String, Object> param = new HashMap<String, Object>();
         param.put("name", playerName);
         Player p = database.getSingleResult(Player.class, "SELECT p FROM Player p WHERE p.userName=:name", param);
