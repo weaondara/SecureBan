3b70086774145cdc428892f3d9fc71fbc7b24a91
dailycraft <server@daily-craft.de>
2013-02-09 22:57:57 +0100

    lets wiring this fucking server

---
+++ BungeePlugin/src/main/java/de/minecraftadmin/secureban/jumpstring/JumpString.java
@@ -0,0 +1,22 @@
+package de.minecraftadmin.secureban.jumpstring;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dustin
+ * Date: 09.02.13
+ * Time: 22:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class JumpString extends Plugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
