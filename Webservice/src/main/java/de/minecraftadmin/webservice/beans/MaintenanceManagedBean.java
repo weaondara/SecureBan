3bb8fd0e71b7f0e7b5d62af1482adfda60c12dc2
dailycraft <server@daily-craft.de>
2013-02-21 22:56:03 +0100

    add maintenance messages via web ui

--- Webservice/src/main/java/de/minecraftadmin/webservice/beans/MaintenanceManagedBean.java
+++ Webservice/src/main/java/de/minecraftadmin/webservice/beans/MaintenanceManagedBean.java
@@ -5,6 +5,8 @@ import de.minecraftadmin.ejb.beans.MaintenanceService;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,13 +16,18 @@ import java.util.List;
  * Time: 19:41
  * To change this template use File | Settings | File Templates.
  */
-@ManagedBean
+@ManagedBean(name = "MaintenanceManager")
+@SessionScoped
 public class MaintenanceManagedBean {
 
+    private Long id;
+    private Date start;
+    private Date end;
+    private String message;
     @EJB
     private MaintenanceService maintenanceService;
 
-    public List<Maintenance> getAllMaintenance() {
+    public List<Maintenance> allMaintenance() {
         return maintenanceService.getAllMaintenance();
     }
 
@@ -29,6 +36,43 @@ public class MaintenanceManagedBean {
     }
 
     public void addMaintenance() {
+        Maintenance maintenance = new Maintenance();
+        maintenance.setId(id);
+        maintenance.setStartTime(start.getTime());
+        maintenance.setEndTime(end.getTime());
+        maintenance.setMessage(message);
+        maintenanceService.addMaintenance(maintenance);
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
