6ba290cffcc33a1592cb2f7edf112cea485c85e4
dailycraft <server@daily-craft.de>
2013-04-18 23:08:47 +0200

    use always production mode for public usage

--- WebInterface/src/main/php/index.php
+++ WebInterface/src/main/php/index.php
@@ -19,7 +19,7 @@ date_default_timezone_set('Etc/GMT-1');
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'production');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
