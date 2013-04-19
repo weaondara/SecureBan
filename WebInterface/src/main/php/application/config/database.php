115ac84bc2c4ba5b90ec7633a17a2a37e71daa7d
dailycraft <server@daily-craft.de>
2013-04-19 23:21:32 +0200

    prepare database configuration file so it can be completed by installation php script

--- WebInterface/src/main/php/application/config/database.php
+++ WebInterface/src/main/php/application/config/database.php
@@ -48,12 +48,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'secureban';
+//$db['default']['hostname'] = 'localhost';
+//$db['default']['username'] = 'root';
+//$db['default']['password'] = '';
+//$db['default']['database'] = 'secureban';
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+//$db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
@@ -63,7 +63,3 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
-
-/* End of file database.php */
-/* Location: ./application/config/database.php */
