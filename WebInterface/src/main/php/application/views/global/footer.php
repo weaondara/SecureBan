51eb2aed4b091238b9095d918b718f9a7498a940
dailycraft <server@daily-craft.de>
2013-04-19 23:17:02 +0200

    keep in mind modified this webinterface by me base codeigniter / keks_commander

--- WebInterface/src/main/php/application/views/global/footer.php
+++ WebInterface/src/main/php/application/views/global/footer.php
@@ -1,8 +1,8 @@
 <div style="clear: both;"></div>
 </div>
 <div id="footer">
-    Web-Interface by Commander_Keks<br/>
-    SecureBan by BADMAN152, DrNoD
+    Web-Interface by Commander_Keks, Modified by BADMAN152<br/>
+    SecureBan by BADMAN152
 </div>
 </body>
 </html>
